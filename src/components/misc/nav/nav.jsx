82969eb385a24ca15d24f18457391f85c0aabcb3
pazcualina <tamarasancristobal@gmail.com>
2026-06-13 11:42:11 -0300

    fix: nav typo

--- src/components/misc/nav/nav.jsx
+++ src/components/misc/nav/nav.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import styles from './Nav.module.css';
+import styles from './nav.module.css';
 
 const Nav = () => {
   const [active, setActive] = React.useState('hero');
